04384b0766f2de0365f50880ee1353ab3690e9f6
Chillax MC <ChillaxOwner@gmail.com>
2022-03-25 21:43:46 -0400

    Fixed PNG resolution and some math

--- src/main/java/com/chillaxmc/customnpcex/client/gui/WantedOverlay.java
+++ src/main/java/com/chillaxmc/customnpcex/client/gui/WantedOverlay.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class WantedOverlay extends Gui {
 
     private final ResourceLocation bar = new ResourceLocation(Reference.MOD_ID, "textures/gui/wantedbar.png");
-    private final int tex_width = 100, tex_height = 20;
+    private final int tex_width = 64, tex_height = 13;
 
     @SubscribeEvent
     public void renderOverlay(RenderGameOverlayEvent event){
@@ -21,8 +21,8 @@ public class WantedOverlay extends Gui {
 
             float oneUnit = (float) tex_width / 5;
             int currentWidth = (int) (oneUnit * 2);
-            drawTexturedModalRect(580,5,0,0, tex_width, tex_height);
-            drawTexturedModalRect(580,5,0, tex_height, currentWidth, tex_height);
+            drawTexturedModalRect(event.getResolution().getScaledWidth()-tex_width-20,10,0,0, tex_width, tex_height);
+            drawTexturedModalRect(event.getResolution().getScaledWidth()-tex_width-20,10,0, tex_height, currentWidth, tex_height);
 
         }
     }
